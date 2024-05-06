f391d22e65b32c7bbfbdbe5523c7b978ed146f8a
osmar <osmarrod18@gmail.com>
2024-05-06 23:50:31 +0100

    updated create project

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -53,7 +53,7 @@ export default function Index({auth, projects, queryParams = null, success}) {
                         Projects
                     </h2>
                     <Link
-
+                        href={route("projects.create")}
                         className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600"
                     >
                         Add new
