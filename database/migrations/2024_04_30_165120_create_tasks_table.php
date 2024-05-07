e7f3d72f5486aac214b70d63b2839995c3e47c36
osmar <osmarrod18@gmail.com>
2024-05-07 18:47:37 +0100

    updated task migration

--- database/migrations/2024_04_30_165120_create_tasks_table.php
+++ database/migrations/2024_04_30_165120_create_tasks_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,10 +14,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->longText('description')->nullable();
-            $table->string('image_path')->nullable();;
+            $table->string('image_path')->nullable();
             $table->foreignId('assigned_user_id')->constrained('users');
             $table->foreignId('project_id')->constrained('projects');
             $table->enum('priority', ['low', 'medium', 'high'])->default('low');
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
             $table->string('status')->nullable();
             $table->timestamps();
         });
