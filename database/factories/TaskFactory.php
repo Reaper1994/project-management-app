03de8cf06be49fbf732a36807ec02e7da7a55798
osmar <osmarrod18@gmail.com>
2024-05-07 19:04:59 +0100

    updated factories and routes

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,10 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ * @extends Factory<Task>
  */
 class TaskFactory extends Factory
 {
@@ -21,9 +23,11 @@ class TaskFactory extends Factory
             'description' => $this->faker->text(),
             'image_path' => $this->faker->imageUrl(),
             'assigned_user_id' => 1,
-            'status' => $this->faker->randomElement(['pending','in_progress','completed']),
-            'project_id' => $this->faker->randomElement([1,2,3,4,5,6,7,8,9]),
+            'status' => $this->faker->randomElement(['pending', 'in_progress', 'completed']),
+            'project_id' => $this->faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9]),
             'priority' => $this->faker->randomElement(['low', 'medium', 'high']),
+            'created_by' => User::factory()->create()->id,
+            'updated_by' => User::factory()->create()->id,
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,15 +23,8 @@ Route::redirect('/', 'Dashboard');
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', fn() => Inertia::render('Dashboard'))->name('dashboard');
     Route::resource('tasks', TaskController::class);
-//    Route::resource('projects', ProjectController::class);
+    Route::resource('projects', ProjectController::class);
     Route::resource('users', UserController::class);
-
-    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
-    Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
-    Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
-    Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
-    Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
-    Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
 });
 
 Route::middleware('auth')->group(function () {
