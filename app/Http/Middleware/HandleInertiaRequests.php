0f5008cc5a02c3bfe037deb7bae34eac9cc47b29
osmar <osmarrod18@gmail.com>
2024-05-24 15:13:09 +0100

    refactored name

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -43,7 +43,7 @@ class HandleInertiaRequests extends Middleware
             ],
             'statuses' => [
                 'status' => StatusEnum::cases(),
-                'TaskPriorityStatus' => TaskPriorityEnum::cases(),
+                'task_priority_status' => TaskPriorityEnum::cases(),
             ]
         ];
     }
